2a55706832bb6e7962b70713dcb5d7ca55d51d1f
rukhsora31 <ruhsora447@gmail.com>
2024-08-17 16:52:20 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// const color = prompt('Choose color: red, blue or green')
+
+// const box = document.querySelector('.box')
+// console.log(box);
+
+// if (color === 'red') {
+//     box.classList.add('red')
+// } else if (color === 'green') {
+//     box.classList.add('green')
+// } else if(color === 'blue') {
+//     box.classList.add('blue')
+// } else {
+//     alert('вы ввели некорректный цвет!')
+// }
+ 
+// const title = document.querySelector('h1')
+// const newText = prompt('введите новый текст для зоголовка:')
+
+// if(newText) {
+//     title.textContent = newText
+// } else {
+//     alert('вы ничего не ввели.')
+// }
+  
+
+// const name = document.querySelector('.box h1')
+// const surname = document.querySelector('.box h2')
+// const age = document.querySelector('.box h3')
+// const image = document.querySelector('.box img')
+
+// const firstName = prompt('your name:')
+// const lastName = prompt('your surname:')
+// const enterAge = prompt('Your age')
+// const enterImage = prompt('your image')
+
+// name.textContent = firstName
+// surname.textContent = lastName
+// age.textContent = enterAge
+// image.src = enterImage
+
